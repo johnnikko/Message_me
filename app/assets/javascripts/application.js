b057791295463f2c58affc3ff34ee792e469365e
johnnikko <nikko06albarado@gmail.com>
2019-08-12 14:21:03 +0800

    add icon and update navigation bar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,12 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
 //= require activestorage
+//= require semantic-ui
 //= require turbolinks
 //= require_tree .
+
+$(document).on('turbolinks:load',function(){
+    $('.ui.dropdown').dropdown(0);
+})
